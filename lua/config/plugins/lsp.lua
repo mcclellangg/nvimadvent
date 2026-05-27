cce233bd745dcf424a9b0897b77c2ff0fbf0aa54
grayson@vera <mcclellangg@gmail.com>
2026-05-27 16:24:26 -0400

    add legacy lspconfig;

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,10 +1,23 @@
 -- plugins/lsp.lua
 return {
   {
+    dependencies = {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
     -- just by typing this in it clones the repo
     "https://github.com/neovim/nvim-lspconfig",
     config = function()
-    
+      -- LEGACY require, to be depreceated
+      require('lspconfig').lua_ls.setup {}
+      -- vim.lsp.config('lua_ls', {})
     end,
   }
 }

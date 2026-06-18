575a32cb6ef84a15969f1d204011bf9f9f22c42b
grayson@vera <mcclellangg@gmail.com>
2026-06-18 16:17:23 -0400

    oil: enable display hidden files

--- lua/config/plugins/oil.lua
+++ lua/config/plugins/oil.lua
@@ -1,13 +1,17 @@
 return {
   {
+    enabled = true,
     'stevearc/oil.nvim',
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    opts = {},
-    -- Optional dependencies
     dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
+    config = function()
+      local oil = require("oil")
+      oil.setup({
+        view_options = {
+          -- Show files and directories that start with "."
+          show_hidden = true,
+        }
+      })
+    end
   }
 }
